031c26bda725e3bdb89946ff5f05dd9d9468ece1
Nicholas Blumhardt <nblumhardt@nblumhardt.com>
2019-03-13 10:24:04 +1000

    Always establish a new connection for every health check

--- src/Seq.Input.HealthCheck/HttpHealthCheck.cs
+++ src/Seq.Input.HealthCheck/HttpHealthCheck.cs
@@ -22,6 +22,7 @@ namespace Seq.Input.HealthCheck
             _title = title ?? throw new ArgumentNullException(nameof(title));
             _targetUrl = targetUrl ?? throw new ArgumentNullException(nameof(targetUrl));
             _httpClient = new HttpClient();
+            _httpClient.DefaultRequestHeaders.Connection.Add("Close");
         }
 
         public async Task<HealthCheckResult> CheckNow(CancellationToken cancel)
