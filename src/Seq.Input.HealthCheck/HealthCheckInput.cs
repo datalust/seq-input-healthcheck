59598b8db38da65c7aa71fdd6d1b834ac71aa2d3
Leigh Metzroth <leigh.metzroth@rpsgroup.com.au>
2024-07-29 10:19:52 +1000

    Changes the delayed start to be based on the number of target urls dividing the interval into the number of targets so that they are evenly spaced along the interval

--- src/Seq.Input.HealthCheck/HealthCheckInput.cs
+++ src/Seq.Input.HealthCheck/HealthCheckInput.cs
@@ -15,6 +15,7 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Net.Http;
 using Seq.Apps;
 using Seq.Input.HealthCheck.Data;
@@ -70,13 +71,6 @@ public class HealthCheckInput : SeqApp, IPublishJson, IDisposable
     public int IntervalSeconds { get; set; } = 60;
 
     [SeqAppSetting(
-        DisplayName = "Jitter (seconds)",
-        IsOptional = true,
-        HelpText = "Adds a random amount of jitter to the running of the first check, to prevent all health checks " +
-                   "from occurring simultaneously; the default is 0.")]
-    public int JitterSeconds { get; set; } = 0;
-
-    [SeqAppSetting(
         DisplayName = "Data extraction expression",
         IsOptional = true,
         HelpText = "A Seq query language expression used to extract information from JSON responses. " +
@@ -94,10 +88,9 @@ public class HealthCheckInput : SeqApp, IPublishJson, IDisposable
         if (!string.IsNullOrWhiteSpace(DataExtractionExpression))
             extractor = new JsonDataExtractor(DataExtractionExpression);
 
-        var random = new Random();
-
         var targetUrls = TargetUrl.Split(new[] {'\r', '\n'}, StringSplitOptions.RemoveEmptyEntries);
-        foreach (var targetUrl in targetUrls)
+
+        foreach (var (targetUrl, ix) in targetUrls.Select((t, i) => (t, i)))
         {
             var healthCheck = new HttpHealthCheck(
                 _httpClient,
@@ -108,10 +101,14 @@ public class HealthCheckInput : SeqApp, IPublishJson, IDisposable
                 BypassHttpCaching,
                 FollowRedirects);
 
+            var delayStart = targetUrls.Length <= 1
+                ? TimeSpan.Zero
+                : TimeSpan.FromSeconds(((IntervalSeconds * 1000.0) / targetUrls.Length) * ix);
+
             _healthCheckTasks.Add(new HealthCheckTask(
                 healthCheck,
                 TimeSpan.FromSeconds(IntervalSeconds),
-                JitterSeconds == 0 ? TimeSpan.Zero : TimeSpan.FromMilliseconds(random.Next(0, JitterSeconds * 1000)),
+                delayStart,
                 reporter,
                 Log));
         }
